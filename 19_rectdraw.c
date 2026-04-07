462584853b76bcc1c752e08ebedfa1be19fb7512
shreeshtripathi <shreeshtripathi3@gmail.com>
2026-04-07 17:01:16 +0530

    add rectdraw.c

---
+++ 19_rectdraw.c
@@ -0,0 +1,25 @@
+//Prompt a user to insert a symbol and length and width and then draw a rectangle using that symbol. The rectangle should be specified characters wide and specified characters tall.
+
+#include <stdio.h>
+int main() {
+    char symbol;
+    int width, height;
+
+    printf("Enter a symbol to draw the rectangle: ");
+    scanf("%c", &symbol);
+
+    printf("Enter the width of the rectangle: ");
+    scanf("%d", &width);
+
+    printf("Enter the height of the rectangle: ");
+    scanf("%d", &height);
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            printf("%c ", symbol);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
