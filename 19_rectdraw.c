4a1acd88e3809d5d8aa7014966d693a85b9b9029
shreeshtripathi <shreeshtripathi3@gmail.com>
2026-04-08 15:29:15 +0530

    clearbuffer in 19

--- 19_rectdraw.c
+++ 19_rectdraw.c
@@ -6,13 +6,16 @@ int main() {
     int width, height;
 
     printf("Enter a symbol to draw the rectangle: ");
-    scanf("%c", &symbol);
+    scanf(" %c", &symbol);
+    clearBuffer(); // Clear the input buffer to avoid issues with subsequent inputs
 
     printf("Enter the width of the rectangle: ");
     scanf("%d", &width);
+    clearBuffer(); // Clear the input buffer to avoid issues with subsequent inputs
 
     printf("Enter the height of the rectangle: ");
     scanf("%d", &height);
+    clearBuffer(); // Clear the input buffer to avoid issues with subsequent inputs
 
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
