1d0a4af24216acc0b16f9a00145ebb7e863dd34d
shreeshtripathi <shreeshtripathi3@gmail.com>
2026-04-26 17:30:34 +0530

    add enums

--- 30_enums.c
+++ 30_enums.c
@@ -1,8 +1,17 @@
 //enums demo
 #include <stdio.h>
-enum Day { SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY };
+typedef enum {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+} Day;
+
 int main() {
-    enum Day myDay = TUESDAY;
+    Day myDay = TUESDAY;
     printf("The day is: %d\n", myDay); // Output will be 2 since TUESDAY is the third value in the enum
     return 0;
 }
